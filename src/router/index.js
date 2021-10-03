5f15a455b64b16b00905cfaaf63e7cc6a7b6069b
lukaCvitanovic <lcvita01@fesb.hr>
2021-10-03 22:58:39 +0200

    Added List and Details views with routing - Added CommonLayout to render basic page layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,28 @@
 import VueRouter from 'vue-router';
 import Vue from 'vue';
 
-const routes = [];
-
 const router = new VueRouter({
   mode: 'history',
   caseSensitive: true,
-  routes,
+  routes: [
+    {
+      path: '/',
+      name: 'root',
+      component: () => import('@/views/common/CommonLayout'),
+      children: [
+        {
+          path: '/',
+          name: 'users',
+          component: () => import('@/views/List'),
+        },
+        {
+          path: 'users/:id(\\d+)',
+          name: 'user',
+          component: () => import('@/views/Details'),
+        },
+      ],
+    },
+  ],
 });
 
 Vue.use(VueRouter);

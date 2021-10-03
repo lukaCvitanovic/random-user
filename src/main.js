8e4f5a0769b7f1035e3199539888ab266a3f3e50
lukaCvitanovic <lcvita01@fesb.hr>
2021-10-03 22:11:07 +0200

    Added axios to project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue';
 import App from './App.vue';
+import axios from 'axios';
 import '@/assets/styles/tailwind.css';
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$api = axios.create({
+  baseURL: 'https://randomuser.me/api/',
+  timeout: 15000,
+  defaults: {
+    headers: {
+      common: {
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+    },
+  },
+});
+Vue.prototype.$api.interceptors.response.use((config) => config, (error) => Promise.reject(error));
 
 new Vue({
   render: h => h(App),

5e1283ba471bf5258e2cf971a8376eb5f2a0942b
lukaCvitanovic <lcvita01@fesb.hr>
2021-10-03 23:22:26 +0200

    Added common header

---
+++ src/components/common/CommonHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full flex justify-center items-center py-3 border-b border-black">
+    <router-link :to="{ name: 'root' }">
+      <img
+        :src="require('@/assets/logo.svg')"
+        class="h-5"
+      >
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonHeader',
+};
+</script>
--- src/views/common/CommonLayout.vue
+++ src/views/common/CommonLayout.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-    CommonLayout
-    <router-view />
+  <div class="w-full flex flex-col">
+    <common-header />
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
+import CommonHeader from '@/components/common/CommonHeader';
+
 export default {
   name: 'CommonLayout',
+
+  components: {
+    CommonHeader,
+  },
 };
 </script>

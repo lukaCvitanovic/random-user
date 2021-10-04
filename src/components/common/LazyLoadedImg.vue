618382e379da373f2cbffc101e6a727595c7a8a5
lukaCvitanovic <lcvita01@fesb.hr>
2021-10-05 00:03:01 +0200

    Added lazy loading images - Added loding spinner gif - Added LazyLoadedImg thin wraper around img - Replaced img wiht LazyLoadedImg component

---
+++ src/components/common/LazyLoadedImg.vue
@@ -0,0 +1,20 @@
+<template>
+  <img
+    v-lazy="src"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+</template>
+
+<script>
+export default {
+  name: 'LazyLoadedImg',
+
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
